6ba951a21384c93aedb4d132fc9c4d08a268e90b
Ganesan Perumalsamy <Ganesan.Perumalsamy@dell.com>
2022-01-13 12:12:49 +0530

    added project

---
+++ TutorialApplication/TutorialApplication/Index.aspx.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using TutorialApplication.DataConnection;
+using System.Data;
+
+namespace TutorialApplication
+{
+    public partial class Index : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void btnSave_Click(object sender, EventArgs e)
+        {
+            TutorialDBConnection tutorialDBConnectionObj = new TutorialDBConnection();
+            TutorialModel tutorialModel = new TutorialModel();
+
+            tutorialModel.TutorialId =Convert.ToInt32(txtTutorialId.Text);
+            tutorialModel.TutorialName = txtTutorialName.Text;
+            tutorialModel.TutorialDesc = txtTutorialDesc.Text;
+            tutorialModel.Published = Convert.ToInt32(txtPublished.Text);
+
+            string msg = tutorialDBConnectionObj.InsertTutorial(tutorialModel);
+            lblResult.Text = msg;
+
+            DataTable dtResult = tutorialDBConnectionObj.SelectTutorial();
+            gvTutorialDetails.DataSource = dtResult;
+            gvTutorialDetails.DataBind();
+        }
+
+        protected void btnReset_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
